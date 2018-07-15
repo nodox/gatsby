a1d8b31db38c6de006239eb8ee52042a9120b9c6
nodox <snatera09@gmail.com>
2018-07-14 23:43:05 -0400

    add copy to differentiate between the themes

--- examples/using-themes/themes/using-themes-pkg/src/pages/index.js
+++ examples/using-themes/themes/using-themes-pkg/src/pages/index.js
@@ -5,9 +5,8 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+    <h1>we are viewing a new theme</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
