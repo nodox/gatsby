433592b2a3e7aa054f5c0f675c13571be5a2083e
nodox <snatera09@gmail.com>
2018-07-14 23:40:42 -0400

    add a new themes package

---
+++ examples/using-themes/themes/using-themes-pkg/gatsby-themes.js
@@ -0,0 +1,13 @@
+module.exports = {
+  enabled: 'gatsby-starter-dimension'
+  themes: [
+    {
+      resolve: 'gatsby-starter-dimension',
+      options: {
+        menu: {
+          color: 'red'
+        },
+      },
+    }
+  ],
+}
