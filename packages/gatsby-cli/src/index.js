38f2e4ad46973e62595e4229e06f0af7184d14ec
nodox <snatera09@gmail.com>
2018-07-17 00:10:16 -0400

    WIP use master node to spwn worker nodes
    the worker nodes will run the express server then die, once the theme
    needs to be changed.

--- packages/gatsby-cli/src/index.js
+++ packages/gatsby-cli/src/index.js
@@ -1,42 +1,21 @@
 #!/usr/bin/env node
 
-// babel-preset-env doesn't find this import if you
-// use require() with backtick strings so use the es6 syntax
-import "babel-polyfill"
+const { fork } = require('child_process')
+const cmd = process.argv.join(" ")
+console.log(cmd);
 
-const createCli = require(`./create-cli`)
-const report = require(`./reporter`)
 
-global.Promise = require(`bluebird`)
+// spawn a process with default args
+const child = fork(cmd)
 
-const version = process.version
-const verDigit = Number(version.match(/\d+/)[0])
+child.on('message', (msg) => {
+  console.log(msg);
+})
 
-const pkg = require(`../package.json`)
-const updateNotifier = require(`update-notifier`)
-// Check if update is available
-updateNotifier({ pkg }).notify()
-
-if (verDigit < 4) {
-  report.panic(
-    `Gatsby 1.0+ requires node.js v4 or higher (you have ${version}). \n` +
-      `Upgrade node to the latest stable release.`
-  )
-}
-
-Promise.onPossiblyUnhandledRejection(error => {
-  report.error(error)
+process.on('unhandledRejection', error => {
   throw error
 })
 
-process.on(`unhandledRejection`, error => {
-  // This will exit the process in newer Node anyway so lets be consistent
-  // across versions and crash
-  report.panic(`UNHANDLED REJECTION`, error)
+process.on('uncaughtException', error => {
+  throw error
 })
-
-process.on(`uncaughtException`, error => {
-  report.panic(`UNHANDLED EXCEPTION`, error)
-})
-
-createCli(process.argv)
